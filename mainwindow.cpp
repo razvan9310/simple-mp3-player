2965a2c50aa67ca5fc88aa09a8f40ecb1dafb011
razvan9310 <razvan.damachi@gmail.com>
2014-12-08 23:32:29 +0200

    Adding seek bar and duration.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,28 +5,140 @@ MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent),
   ui(new Ui::MainWindow) {
   ui->setupUi(this);
-  setWindowTitle("Simple Media Player");
+  setWindowTitle("Simple MP3 Player");
 
   player = new QMediaPlayer;
   player->setVolume(70);
+  connect(player, SIGNAL(durationChanged(qint64)), this, SLOT(durationChanged(qint64)));
+  connect(player, SIGNAL(stateChanged(QMediaPlayer::State)), this, SLOT(stateChanged(QMediaPlayer::State)));
+  connect(player, SIGNAL(positionChanged(qint64)), this, SLOT(positionChanged(qint64)));
+
+  slider = findChild<QSlider *>("slider");
+  slider->setRange(0, 0);
+  title = findChild<QLabel *>("title");
+  current_time = findChild<QLabel *>("current_time");
+  duration = findChild<QLabel *>("duration");
 }
 
 MainWindow::~MainWindow() {
+  delete duration;
+  delete current_time;
+  delete title;
+  delete slider;
+  delete player;
   delete ui;
 }
 
 void MainWindow::on_openFileButton_clicked() {
   song_path = QFileDialog::getOpenFileName(this,tr("Choose Song"),
-      QDir::currentPath(),
+      song_path.isNull() ? QDir::currentPath() : song_path,
       tr("MP3 Files (*.mp3)"));
+  if (song_path == 0) {
+    return;
+  }
   player->setMedia(QUrl::fromLocalFile(song_path));
+  player->play();
+  paused = false;
+  title->setText(simple_file_name(song_path));
+  slider->setValue(0);
 }
 
 void MainWindow::on_playPauseButton_clicked() {
-    if (player->state() == QMediaPlayer::PausedState
-        || player->state() == QMediaPlayer::StoppedState) {
+    if (player->state() != QMediaPlayer::PlayingState) {
+      player->setPosition(slider->value() * 1000);
       player->play();
+      paused = false;
     } else {
       player->pause();
+      paused = true;
     }
+}
+
+void MainWindow::durationChanged(qint64 new_duration) {
+  slider->setRange(0, new_duration / 1000);
+  QString duration_string = " /  ";
+  duration_string.append(time_string_from_millis(new_duration));
+  duration->setText(duration_string);
+}
+
+void MainWindow::on_slider_sliderMoved(int seconds) {
+  player->setPosition(seconds * 1000);
+}
+
+void MainWindow::on_slider_sliderPressed() {
+  player->pause();
+}
+
+void MainWindow::on_slider_sliderReleased() {
+  if (!paused) {
+    player->play();
+  }
+}
+
+QString MainWindow::simple_file_name(const QString &file_path) {
+  std::string file_path_string = file_path.toStdString();
+  int pos = file_path_string.size() - 1;
+  while (pos >= 0 && file_path_string[pos] != '/'
+         && file_path_string[pos] != '\\') {
+    --pos;
+  }
+  return QString::fromStdString(file_path_string.substr(pos + 1));
+}
+
+void MainWindow::stateChanged(QMediaPlayer::State state) {
+  switch (state) {
+    case QMediaPlayer::StoppedState:
+      slider->setValue(0);
+      current_time->setText("00:00:00");
+      break;
+    default:
+      return;
+  }
+}
+
+QString MainWindow::time_string_from_millis(const qint64 &millis) {
+  unsigned seconds = millis / 1000;
+  unsigned hh = seconds / 3600;
+  unsigned mm =(seconds % 3600) / 60;
+  unsigned ss = (seconds % 3600) % 60;
+  QString duration_string = QString("");
+  if (hh < 10) {
+    duration_string.append('0');
+  } else {
+    duration_string.append(hh / 10 + '0');
+  }
+  duration_string.append(hh % 10 + '0');
+  duration_string.append(':');
+  if (mm < 10) {
+    duration_string.append('0');
+  } else {
+    duration_string.append(mm / 10 + '0');
+  }
+  duration_string.append(mm % 10 + '0');
+  duration_string.append(':');
+  if (ss < 10) {
+    duration_string.append('0');
+  } else {
+    duration_string.append(ss / 10 + '0');
+  }
+  duration_string.append(ss % 10 + '0');
+  return duration_string;
+}
+
+void MainWindow::positionChanged(qint64 position) {
+  slider->setValue(position / 1000);
+  current_time->setText(time_string_from_millis(position));
+}
+
+void MainWindow::on_slider_actionTriggered(int action) {
+  switch (action) {
+    case QAbstractSlider::SliderPageStepAdd:
+      player->setPosition(player->position() + slider->pageStep() * 1000);
+      break;
+    case QAbstractSlider::SliderPageStepSub:
+      player->setPosition(player->position() - slider->pageStep() * 1000);
+      break;
+    default:
+      return;
+  }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,8 @@
 #include <QtMultimedia/QMediaPlayer>
 #include <QFileDialog>
 #include <QString>
+#include <QSlider>
+#include <QLabel>
 
 namespace Ui {
   class MainWindow;
@@ -19,13 +21,28 @@ public:
 
 private slots:
   void on_openFileButton_clicked();
-
   void on_playPauseButton_clicked();
+  void durationChanged(qint64);
+  void on_slider_sliderMoved(int);
+  void on_slider_sliderPressed();
+  void on_slider_sliderReleased();
+  void stateChanged(QMediaPlayer::State);
+  void positionChanged(qint64);
+
+  void on_slider_actionTriggered(int action);
 
 private:
+  QString simple_file_name(const QString &);
+  QString time_string_from_millis(const qint64 &);
+
   Ui::MainWindow *ui;
   QMediaPlayer *player;
+  QSlider *slider;
+  QLabel *title;
+  QLabel *current_time;
+  QLabel *duration;
   QString song_path;
+  bool paused;
 };
 
 #endif // MAINWINDOW_H
